d251940d874b407ecfacc345f00bea090b0fd710
Cripler1993 <cripler1993@mail.ru>
2024-03-26 22:09:57 +0300

    styling modal wrapper, work with timer logic

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let tariffs = document.querySelectorAll("#tariffs");
+let minute = document.querySelector("#minutes");
+let second = document.querySelector("#seconds");
+let timeCircles = document.querySelectorAll(".time__circle");
 
 tariffs.forEach((elem) => {
   elem.addEventListener("click", addActiveTariff);
@@ -16,3 +19,27 @@ function clearAll() {
     elem.classList.add("non__active-tariff");
   });
 }
+
+let time = 120;
+
+let timerId = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  minute.innerHTML = `0${minutes}`;
+  second.innerHTML = `${seconds}`;
+  time--;
+  if (time == -1) {
+    clearTimeout(timerId);
+  } else if (time == 29) {
+    minute.classList.add("time__numbers-red");
+    second.classList.add("time__numbers-red");
+    timeCircles.forEach((elem) => {
+      elem.classList.add("time__circle-red");
+    });
+  }
+}
+
+// остановка счетчика
