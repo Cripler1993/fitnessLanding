9dd2243fe93cf7290b80aa4f561828c9e939d0e3
Cripler1993 <cripler1993@mail.ru>
2024-03-28 22:03:15 +0300

    adaptive styling and js logic

--- script.js
+++ script.js
@@ -2,6 +2,64 @@ let tariffs = document.querySelectorAll("#tariffs");
 let minute = document.querySelector("#minutes");
 let second = document.querySelector("#seconds");
 let timeCircles = document.querySelectorAll(".time__circle");
+let modalWrapper = document.querySelector(".modal__wrapper");
+let discountItems = document.querySelectorAll(".discount__item");
+let modalClose = document.querySelector(".modal__close");
+
+let time = 1;
+
+let allTariffs = {};
+
+const url = "https://t-pay.iqfit.app/subscribe/list-test";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    allTariffs = data.reduce((acc, elem) => {
+      if (elem.name in acc) {
+        acc[elem.name].push(elem);
+      } else {
+        acc[elem.name] = [elem];
+      }
+      return acc;
+    }, {});
+    renderTariffs(allTariffs, true);
+  });
+
+function renderTariffs(obj, isDiscount) {
+  tariffs.forEach((elem) => {
+    let [tariffWithDiscount, tariffWithoutDiscount, modalTariff] =
+      obj[elem.dataset.key];
+    if (isDiscount) {
+      elem.querySelector(
+        ".with__discount"
+      ).innerHTML = `${tariffWithDiscount.price}₽`;
+      elem.querySelector(
+        ".without__discount"
+      ).innerHTML = `${tariffWithoutDiscount.price}₽`;
+    } else {
+      elem.querySelector(
+        ".with__discount"
+      ).innerHTML = `${tariffWithoutDiscount.price}₽`;
+      elem.querySelector(".without__discount").classList.add("hide");
+      elem.querySelector(".item__discount").classList.add("hide");
+    }
+    let modal = modalWrapper.querySelector(
+      '[data-key="' + elem.dataset.key + '"]'
+    );
+    if (modal) {
+      modal.querySelector(
+        ".discount__item-price p"
+      ).innerHTML = `${modalTariff.price}₽`;
+      modal.querySelector(
+        ".without__price"
+      ).innerHTML = `${tariffWithoutDiscount.price}₽`;
+    }
+  });
+}
+// тарифы на главной странице
 
 tariffs.forEach((elem) => {
   elem.addEventListener("click", addActiveTariff);
@@ -20,7 +78,7 @@ function clearAll() {
   });
 }
 
-let time = 120;
+// таймер
 
 let timerId = setInterval(updateCountdown, 1000);
 
@@ -33,7 +91,9 @@ function updateCountdown() {
   time--;
   if (time == -1) {
     clearTimeout(timerId);
-  } else if (time == 29) {
+    renderTariffs(allTariffs, false);
+    setTimeout(modalOpen, 4000);
+  } else if (time <= 29) {
     minute.classList.add("time__numbers-red");
     second.classList.add("time__numbers-red");
     timeCircles.forEach((elem) => {
@@ -41,5 +101,28 @@ function updateCountdown() {
     });
   }
 }
+// модальное окно
 
-// остановка счетчика
+function modalOpen() {
+  modalWrapper.classList.add("active");
+}
+
+modalClose.addEventListener("click", function () {
+  modalWrapper.classList.add("hidden");
+});
+
+discountItems.forEach((elem) => {
+  elem.addEventListener("click", addModalActiveTariff);
+});
+
+function addModalActiveTariff(event) {
+  clearAllModalTarrifs();
+  let clickedTariff = event.target.closest(".discount__item");
+  clickedTariff.classList.add("discount__item-active");
+}
+
+function clearAllModalTarrifs() {
+  discountItems.forEach((elem) => {
+    elem.classList.remove("discount__item-active");
+  });
+}
